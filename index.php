b0aaa420f6d869ce51282dff5cec7f94a293ab1a
davit <david.grigorianc@gmail.com>
2019-02-07 23:41:41 +0400

    test search

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 require 'classes/Crawler.php';
-    $url = 'https://trovaunposto.it/';
-    $crawler = new Crawler($url);
-    $links = $crawler->init();
-    var_dump($links);
+require 'classes/Model/Page.php';
+
+//    $url = 'https://trovaunposto.it/';
+//    $crawler = new Crawler($url);
+//    $links = $crawler->init();
+//    var_dump($links);
     
+    $page = new Page();
+    $term = 'basta';
+    $a = $page->search($term);
+    var_dump($a);
