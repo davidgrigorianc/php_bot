3f5f36016cca241baa8eb9edee50e143f641dfb5
davit <david.grigorianc@gmail.com>
2019-02-09 15:08:44 +0400

    checking depth

--- classes/Model/Page.php
+++ classes/Model/Page.php
@@ -20,14 +20,14 @@ class Page extends Db{
         return $this->query($query);
     }
     
-    public function savePage($content,$title,$url,$baseUrl,$parent_id){
-        $query = ("INSERT INTO pages(content, title ,url, baseurl ,parent_id) 
-        VALUES('$content','$title','$url','$baseUrl','$parent_id')");
+    public function savePage($content,$title,$url,$baseUrl,$depth,$parent_id){
+        $query = ("INSERT INTO pages(content, title ,url, baseurl ,parent_id, depth) 
+        VALUES('$content','$title','$url','$baseUrl','$parent_id','$depth')");
         return $this->query($query);
     }
     
-    public function getByUrl($param) {
-        $query = "SELECT id FROM pages WHERE url = $url";
+    public function getByUrl($url) {
+        $query = "SELECT id FROM pages WHERE url = '$url' LIMIT 1";
         return $this->query($query);
     }
     
